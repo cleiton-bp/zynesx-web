bb0650695339724d6abdf1d7b5727949d791f472
cleiton-bp <cleitontcpereira@gmail.com>
2025-02-15 10:23:58 -0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  // {
+  //   path: 'in/dashboard',
+  //   loadComponent: () =>
+  //     import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  // }
+];
